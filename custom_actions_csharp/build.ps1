4ad3947ba2d39f3ff90939520043ec9b9593791c
Markus Kramer <markuskramerIgitt@gmail.com>
2020-05-04 22:19:12 +0200

    CheckExitCode

--- custom_actions_csharp/build.ps1
+++ custom_actions_csharp/build.ps1
@@ -1,6 +1,15 @@
 # Set-ExecutionPolicy RemoteSigned
 $msbuild = "C:\Program Files (x86)\MSBuild\14.0\"    # Build tools 2015
-$wixpath = "$($ENV:WIX)bin"
+
+function CheckExitCode($txt) {
+    if ($LastExitCode -eq 0) {
+        Write-Host -ForegroundColor Yellow "$txt done"
+    } else {
+        Write-Host -ForegroundColor Red "$txt failed"
+        exit(1)
+    }
+}
+
 
 Write-Host -ForegroundColor Yellow "Compiling cs into dll..."
 # https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-options/filealign-compiler-option
@@ -9,23 +18,18 @@ $releasedir = "obj\x86\Release"
 cmd /r del /q $releasedir\*.dll
 if (!(Test-Path -Path $releasedir)){New-Item -ItemType directory -Path $releasedir}
 & "$($msbuild)bin\csc.exe" `
- /nologo /noconfig /nowarn:1701,1702 /nostdlib+ /errorreport:prompt /warn:4 /define:TRACE /highentropyva- `
- /reference:"$($ENV:WIX)SDK\Microsoft.Deployment.WindowsInstaller.dll" `
- /reference:"$($ENV:WIX)bin\wix.dll" `
- /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\mscorlib.dll" `
- /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.dll" `
- /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.Xml.dll" `
- /debug:pdbonly /filealign:512 /optimize+ `
- /out:$releasedir\MinionConfigurationExtension.dll `
- /target:library /utf8output `
- MinionConfiguration.cs `
-  Properties\AssemblyInfo.cs 
-if ($LastExitCode -eq 0) {
-  Write-Host -ForegroundColor Yellow "Compiling cs... done"
-} else {
-  Write-Host -ForegroundColor Red "Compiling cs... failed $LastExitCode"
-  exit(1)
-}
+    /nologo /noconfig /nowarn:1701,1702 /nostdlib+ /errorreport:prompt /warn:4 /define:TRACE /highentropyva- `
+    /reference:"$($ENV:WIX)SDK\Microsoft.Deployment.WindowsInstaller.dll" `
+    /reference:"$($ENV:WIX)bin\wix.dll" `
+    /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\mscorlib.dll" `
+    /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.dll" `
+    /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.Xml.dll" `
+    /debug:pdbonly /filealign:512 /optimize+ `
+    /out:$releasedir\MinionConfigurationExtension.dll `
+    /target:library /utf8output `
+    MinionConfiguration.cs `
+    Properties\AssemblyInfo.cs 
+CheckExitCode "Compiling cs"
 Pop-Location
 
 
@@ -34,14 +38,14 @@ Pop-Location
 
 Write-Host -ForegroundColor Yellow "Packing dll's into CA.dll..."
 & "$($ENV:WIX)sdk\MakeSfxCA.exe" `
- "c:\git\LearnWix\custom_actions_csharp\MinionConfigurationExtension\obj\x86\Release\MinionConfigurationExtension.CA.dll" `
- "$($ENV:WIX)sdk\x86\SfxCA.dll" `
- "c:\git\LearnWix\custom_actions_csharp\MinionConfigurationExtension\obj\x86\Release\MinionConfigurationExtension.dll" `
- "$($ENV:WIX)SDK\Microsoft.Deployment.WindowsInstaller.dll" `
- "$($ENV:WIX)bin\wix.dll" `
- "$($ENV:WIX)bin\Microsoft.Deployment.Resources.dll" `
- "c:\git\LearnWix\custom_actions_csharp\MinionConfigurationExtension\CustomAction.config"
-Write-Host -ForegroundColor Yellow "Packing dll's into CA.dll...done"
+    "$pwd\MinionConfigurationExtension\obj\x86\Release\MinionConfigurationExtension.CA.dll" `
+    "$($ENV:WIX)sdk\x86\SfxCA.dll" `
+    "$pwd\MinionConfigurationExtension\obj\x86\Release\MinionConfigurationExtension.dll" `
+    "$($ENV:WIX)SDK\Microsoft.Deployment.WindowsInstaller.dll" `
+    "$($ENV:WIX)bin\wix.dll" `
+    "$($ENV:WIX)bin\Microsoft.Deployment.Resources.dll" `
+    "$pwd\MinionConfigurationExtension\CustomAction.config"
+CheckExitCode "Packing dll's"
 
 
 Write-Host -ForegroundColor Yellow "Collecting files... TODO"
