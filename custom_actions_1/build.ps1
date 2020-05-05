b206bc732fb9992331669c0ae4b0df8f2c6f6cd2
Markus Kramer <markuskramerIgitt@gmail.com>
2020-05-05 16:56:37 +0200

    first msi

--- custom_actions_1/build.ps1
+++ custom_actions_1/build.ps1
@@ -1,4 +1,9 @@
 # Set-ExecutionPolicy RemoteSigned
+
+Set-PSDebug -Strict
+Set-strictmode -version latest
+
+
 $msbuild = "C:\Program Files (x86)\MSBuild\14.0\"    # Build tools 2015
 
 function CheckExitCode($txt) {
@@ -11,7 +16,7 @@ function CheckExitCode($txt) {
 
 Write-Host -ForegroundColor Yellow "Compiling cs into dll..."
 # https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-options/filealign-compiler-option
-Push-Location MinionConfigurationExtension
+Push-Location CA1
 $releasedir = "obj\x86\Release"
 cmd /r del /q $releasedir\*.dll
 if (!(Test-Path -Path $releasedir)){New-Item -ItemType directory -Path $releasedir}
@@ -23,9 +28,9 @@ if (!(Test-Path -Path $releasedir)){New-Item -ItemType directory -Path $released
     /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.dll" `
     /reference:"C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.Xml.dll" `
     /debug:pdbonly /filealign:512 /optimize+ `
-    /out:$releasedir\MinionConfigurationExtension.dll `
+    /out:$releasedir\CA1.dll `
     /target:library /utf8output `
-    MinionConfiguration.cs `
+    CA1.cs `
     Properties\AssemblyInfo.cs 
 CheckExitCode "Compiling cs"
 Pop-Location
@@ -36,13 +41,13 @@ Pop-Location
 
 Write-Host -ForegroundColor Yellow "Packing dll's into CA.dll..."
 & "$($ENV:WIX)sdk\MakeSfxCA.exe" `
-    "$pwd\MinionConfigurationExtension\obj\x86\Release\MinionConfigurationExtension.CA.dll" `
+    "$pwd\CA1\obj\x86\Release\CA1.CA.dll" `
     "$($ENV:WIX)sdk\x86\SfxCA.dll" `
-    "$pwd\MinionConfigurationExtension\obj\x86\Release\MinionConfigurationExtension.dll" `
+    "$pwd\CA1\obj\x86\Release\CA1.dll" `
     "$($ENV:WIX)SDK\Microsoft.Deployment.WindowsInstaller.dll" `
     "$($ENV:WIX)bin\wix.dll" `
     "$($ENV:WIX)bin\Microsoft.Deployment.Resources.dll" `
-    "$pwd\MinionConfigurationExtension\CustomAction.config"
+    "$pwd\CA1\CustomAction.config"
 CheckExitCode "Packing dll's"
 
 
@@ -50,4 +55,28 @@ Write-Host -ForegroundColor Yellow "Collecting files... TODO"
 
 
 
-Write-Host -ForegroundColor Yellow "Compiling wxs... TODO"
+Write-Host -ForegroundColor Yellow "(candle) wxs to wixobj..."
+
+& "$($ENV:WIX)bin\candle.exe" `
+    -nologo `
+    -sw1150 `
+    -ddist=".\" `
+    -ext "$($ENV:WIX)bin\WixUtilExtension.dll" `
+    Product.wxs `
+    CA1CA.wxs  
+CheckExitCode "candle"
+
+
+Write-Host -ForegroundColor Yellow "(light) wixobj to msi..."
+& "$($ENV:WIX)bin\light" -nologo `
+    Product.wixobj `
+    CA1CA.wixobj `
+    -ext "$($ENV:WIX)bin\WixUtilExtension.dll" `
+    -ext "$($ENV:WIX)bin\WixNetFxExtension.dll" `
+    -out Product.msi
+
+#  C:\Program Files (x86)\WiX Toolset v3.11\bin\Light.exe -out C:\git\salt-windows-msi\wix.d\MinionMSI\bin\Release\Salt-Minion-3000.1-Py3-64bit.msi -pdbout C:\git\salt-windows-msi\wix.d\MinionMSI\bin\Release\Salt-Minion-Setup.wixpdb -cultures:null -ext "C:\Program Files (x86)\WiX Toolset v3.11\bin\\WixUtilExtension.dll" -ext "C:\Program Files (x86)\WiX Toolset v3.11\bin\\WixUIExtension.dll" -ext "C:\Program Files (x86)\WiX Toolset v3.11\bin\\WixNetFxExtension.dll" -sice:ICE03 -contentsfile obj\Release\MinionMSI.wixproj.BindContentsFileListnull.txt -outputsfile obj\Release\MinionMSI.wixproj.BindOutputsFileListnull.txt -builtoutputsfile obj\Release\MinionMSI.wixproj.BindBuiltOutputsFileListnull.txt -wixprojectfile C:\git\salt-windows-msi\wix.d\MinionMSI\MinionMSI.wixproj -nologo obj\Release\service.wixobj obj\Release\servicePython.wixobj obj\Release\ProductUIsettings.wixobj obj\Release\MinionConfigurationExtensionCA.wixobj obj\Release\Product.wixobj obj\Release\ProductUI.wixobj obj\Release\dist-amd64.wixobj
+
+CheckExitCode "light"
+
+
